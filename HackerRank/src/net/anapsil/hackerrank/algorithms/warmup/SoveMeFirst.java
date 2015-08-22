041c66a6304427eabc9a108082b422dc499ad090
Ana Paula <anapsil83@gmail.com>
2015-08-22 17:22:27 -0300

    Primeiro problema

---
+++ HackerRank/src/net/anapsil/hackerrank/algorithms/warmup/SoveMeFirst.java
@@ -0,0 +1,22 @@
+package net.anapsil.hackerrank.algorithms.warmup;
+
+import java.util.Scanner;
+
+public class SoveMeFirst {
+	
+	static int sum(int a, int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int a;
+		a = in.nextInt();
+		int b;
+		b = in.nextInt();
+		int sum = sum(a,b);
+		
+		System.out.println(sum);
+	}
+
+}
