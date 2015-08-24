590b2f771047ed87c95fea960a4cd09829b53071
Ana Paula Silva <ana.silva@pontomobi.com.br>
2015-08-24 12:46:04 -0300

    Não finalizado

--- HackerRank/src/net/anapsil/hackerrank/algorithms/warmup/DiagonalDifference.java
+++ HackerRank/src/net/anapsil/hackerrank/algorithms/warmup/DiagonalDifference.java
@@ -1,9 +1,44 @@
 package net.anapsil.hackerrank.algorithms.warmup;
 
+import java.util.Scanner;
+
 public class DiagonalDifference {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		int size = in.nextInt();
+		int matrix[][] = new int[size][size];
+		int firstDiagonal = 0;
+		int secondDiagonal = 0;
+				
+		
+		for (int i = 0; i < size; i++) {
+			if (in.hasNext()) {
+				matrix[0][i] = in.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < size; i++) {
+			if (in.hasNext()) {
+				matrix[1][i] = in.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < size; i++) {
+			if (in.hasNext()) {
+				matrix[2][i] = in.nextInt();
+			}
+		}
+		
+		for (int i = 0; i < size; i++) {
+			firstDiagonal += matrix[i][i];
+		}
+		
+		for (int i = size-1; i == 0; i--) {
+			secondDiagonal += matrix[i][i];
+		}
+		System.out.println(firstDiagonal - secondDiagonal);
 
 	}
 
