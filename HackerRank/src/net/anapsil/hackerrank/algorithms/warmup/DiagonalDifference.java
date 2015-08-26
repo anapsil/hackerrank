4cc176bb92aef1e2ff067fff13aba07f9a5a6699
Ana Paula Silva <ana.silva@pontomobi.com.br>
2015-08-26 09:45:21 -0300

    Correção da solução do problema

--- HackerRank/src/net/anapsil/hackerrank/algorithms/warmup/DiagonalDifference.java
+++ HackerRank/src/net/anapsil/hackerrank/algorithms/warmup/DiagonalDifference.java
@@ -6,34 +6,23 @@ public class DiagonalDifference {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+
 		int size = in.nextInt();
 		int matrix[][] = new int[size][size];
 		int firstDiagonal = 0;
 		int secondDiagonal = 0;
-				
-		
+
 		for (int i = 0; i < size; i++) {
-			if (in.hasNext()) {
-				matrix[0][i] = in.nextInt();
+			for (int j = 0; j < size; j++) {
+				if (in.hasNext()) {
+					matrix[i][j] = in.nextInt();
+				}
 			}
 		}
-		
-		for (int i = 0; i < size; i++) {
-			if (in.hasNext()) {
-				matrix[1][i] = in.nextInt();
-			}
-		}
-		
-		for (int i = 0; i < size; i++) {
-			if (in.hasNext()) {
-				matrix[2][i] = in.nextInt();
-			}
-		}
-		
+
 		for (int i = 0; i < size; i++) {
 			firstDiagonal += matrix[i][i];
-			secondDiagonal += matrix[i][size-1-i];
+			secondDiagonal += matrix[i][size - 1 - i];
 		}
 
 		System.out.println(Math.abs((firstDiagonal - secondDiagonal)));
